0452430224c04c9b840eaf8a489b914e36f07dba
kunal911 <52748248+kunal911@users.noreply.github.com>
2023-12-02 11:44:37 -0700

    Update sort_join.hpp

--- src/include/duckdb/execution/operator/learned_join/algos/sort_join.hpp
+++ src/include/duckdb/execution/operator/learned_join/algos/sort_join.hpp
@@ -20,10 +20,9 @@ namespace learned_index {
             // SortJoin(
             //     SSTable<T> *outer, 
             //     SSTable<T> *inner, 
-            //     IndexBuilder<T> *inner_index_builder,
             //     Comparator<T> *comparator,
-            //     PSSTableBuilder<T> *result_builder,
-            //     int num_threads):
+            //     PSSTableBuilder<T> *result_builder
+					):
             // BaseMergeAndJoinOp<T>(outer, inner, inner_index_builder, comparator, result_builder, num_threads) {}
 
             // static bool dummyDuckDbSortJoin(){
@@ -32,14 +31,6 @@ namespace learned_index {
             // }
 
             // void doOpOnPartition(Partition partition, TableOpResult<T> *result) override {
-            //     uint64_t outer_start = partition.outer.first;
-            //     uint64_t outer_end = partition.outer.second;
-            //     uint64_t inner_start = partition.inner.first;
-            //     uint64_t inner_end = partition.inner.second;
-
-            //     auto outer_iterator = this->outer_->getSSTableForSubRange(outer_start, outer_end)->iterator();
-            //     auto inner_iterator = this->inner_->getSSTableForSubRange(inner_start, inner_end)->iterator();
-            //     auto inner_index = this->inner_index_->getIndexForSubrange(inner_start, inner_end);
             //     auto result_builder = this->result_builder_->getBuilderForRange(inner_start + outer_start, inner_end + outer_end);
 
             //     while (outer_iterator->valid()) {
@@ -54,12 +45,6 @@ namespace learned_index {
             //         }
             //         outer_iterator->next();
             //     }
-
-            //     result->stats["inner_disk_fetch"] = inner_iterator->getDiskFetches();
-            //     result->stats["outer_disk_fetch"] = outer_iterator->getDiskFetches();
             //     result->output_table = result_builder->build(),
-
-            //     delete outer_iterator;
-            //     delete inner_iterator;
             // };
 };
